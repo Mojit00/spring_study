965df825bdf8b26e9d7512cba235d1cf8ec378a8
MrGggger <941997208@qq.com>
2020-04-06 23:46:42 +0800

    调试aware

--- spring-arch-read/src/main/java/com/jitao/service/IndexServiceImpl.java
+++ spring-arch-read/src/main/java/com/jitao/service/IndexServiceImpl.java
@@ -1,11 +1,23 @@
 package com.jitao.service;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
 @Service
-public class IndexServiceImpl implements IndexService {
+public class IndexServiceImpl implements IndexService, ApplicationContextAware {
+
+	private ApplicationContext context;
+
 	@Override
 	public void query() {
 		System.out.println("query");
+		System.out.println(context);
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.context = applicationContext;
 	}
 }
